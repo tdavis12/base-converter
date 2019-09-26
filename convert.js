b0e52384d3ada1a45b4d9915243c1dfbc67aaca3
Tyler Davis <tyler.davis2@kiewit.com>
2019-09-26 09:32:49 -0500

    she works

--- convert.js
+++ convert.js
@@ -2,56 +2,40 @@
 
 function convert(form)
 {
-  var number = form.number.value;
+  var sNumber = form.number.value;
   var cBase = form.currentBase.value;
   var dBase = form.desiredBase.value;
-  var result = 0;
 
-  if (dBase == 10)
+  // Convert to b10
+  var number = parseInt(sNumber, cBase);
+  var result = number;
+
+  if (dBase != 10)
   {
-    result = toTen(number, cBase);
-  }
-  else
-  {
-    if (cBase != 10)
-    {
-      number = toTen(number, cBase);
-    }
     result = fromTen(number, dBase);
   }
 
   alert(result);
 }
 
-function toTen(number, cBase)
-{
-  var result = 0;
-  var numberDigits = [];
-  var sNumber = number.toString();
-  for (var i = 0, l = sNumber.length; i < l; i++)
-  {
-    numberDigits.push(+sNumber.charAt(i));
-  }
-  // Ex: 123b10 -> b5 = 1*5^2 + 2*5^1 +3*5^0
-  for (var i = 0; i < numberDigits.length; i++)
-  {
-    var digit = numberDigits[numberDigits.length - i - 1];
-    var power = Math.pow(cBase, i);
-    result += (digit * power);
-  }
-  return result;
-}
-
 function fromTen(number, dBase)
 {
-  var result = 0;
+  var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
   var pos = 1;
+  var str = "";
   while (number != 0)
   {
     var remainder = number % dBase;
-    result += remainder * pos;
+    if (pos == 1)
+    {
+      str = num[remainder];
+    }
+    else
+    {
+      str = num[remainder] + str;
+    }
     number = (number - remainder) / dBase;
     pos *= 10;
   }
-  return result;
+  return str;
 }
